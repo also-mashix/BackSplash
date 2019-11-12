ea15408f1881a2f265961d809e6b24bff78563d3
Micah <micahak@gmail.com>
2019-11-12 09:50:09 -0500

    Initial commit

---
+++ src/main/java/com/micah/BackSplash/HelloController.java
@@ -0,0 +1,15 @@
+package com.micah.BackSplash;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller 
+public class HelloController {
+
+	@RequestMapping("/greeting")
+	public String displayGreeting() {
+		
+		return "greeting";
+	}
+}
